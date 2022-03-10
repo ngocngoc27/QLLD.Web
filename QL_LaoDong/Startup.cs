ead1b8e5dac238d70c6d2c390b60e045dcdeaae5
HONGNGOC\User 222202 <nguyenthihongngoc703@gmail.com>
2022-03-10 15:46:54 +0700

    []
    Signed-off-by: HONGNGOC\User 222202 <nguyenthihongngoc703@gmail.com>

--- QL_LaoDong/Startup.cs
+++ QL_LaoDong/Startup.cs
@@ -27,30 +27,10 @@ namespace QL_LaoDong
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
-            });
-        }
+      public long IdMn { get; set; }
+        public string Label { get; set; }
+        public long? Parent { get; set; }
+    }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
