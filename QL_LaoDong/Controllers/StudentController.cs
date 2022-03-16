9ec82163882f96df01d15c355221c05f33675c40
HONGNGOC\User 222202 <nguyenthihongngoc703@gmail.com>
2022-03-16 18:33:23 +0700

    []

---
+++ QL_LaoDong/Controllers/StudentController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QL_LaoDong.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QL_LaoDong.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly IStudentService _studentService;
+        private readonly IAccountService _accountService;
+        public StudentController(IStudentService studentService, IAccountService accountService)
+        {
+            _studentService = studentService;
+            _accountService = accountService;
+        }
+        public IActionResult Index()
+        {
+            var data = _studentService.Get();
+            return View(data);
+        }
+    }
+}
